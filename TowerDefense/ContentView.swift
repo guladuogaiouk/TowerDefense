e7da499ff345fd87239324c3d74d2324c9ea44cc
guladuogaiouk <2403939643@qq.com>
2024-06-05 21:29:41 +0800

    Initial Commit

---
+++ TowerDefense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TowerDefense
+//
+//  Created by Tianyu Xu on 2024/6/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TowerDefense/TowerDefenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TowerDefenseApp.swift
+//  TowerDefense
+//
+//  Created by Tianyu Xu on 2024/6/5.
+//
+
+import SwiftUI
+
+@main
+struct TowerDefenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
